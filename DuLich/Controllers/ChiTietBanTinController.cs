48063a6f69bf5c05e0a564bdd894f1b442ef419f
Nguyen Loan <loanloannguyen0038@gmail.com>
2020-06-23 03:46:52 +0700

    Thiết kế giao diện người dùng

---
+++ DuLich/Controllers/ChiTietBanTinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_TinTucDuLich.Controllers
+{
+    public class ChiTietBanTinController : Controller
+    {
+        // GET: ChiTietBanTin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DuLich/Controllers/DanhMucTinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_TinTucDuLich.Controllers
+{
+    public class DanhMucTinController : Controller
+    {
+        // GET: DanhMucTin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
